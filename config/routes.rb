002a3fabb8eb6ac8d0e0bba3b4b004500c97d8d5
santiago herrera <santiherreravelas@gmail.com>
2021-06-15 18:48:49 -0500

    cambios en estilos y autenticacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
   get "/bienvenida", to: "home#index"  #link apunta al controlador home y accion index
