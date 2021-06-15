c222e1ee54a4c601909fdc1a0987a18e85a21be8
santiago herrera <santiherreravelas@gmail.com>
2021-06-15 09:30:01 -0500

    crud funcional con articulos

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, except: [:new, :create,:index]
+
+  def show
+  end
+
+
+  def edit
+  end
+
+
+  def index
+    @articles = Article.all
+  end
+
+
+  def update
+    @article.update(title: params[:article][:title], content: params[:article][:content])
+
+    redirect_to @article
+  end
+
+
+
+  def delete
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+    render json: @article
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+  get "/bienvenida", to: "home#index"  #link apunta al controlador home y accion index
+  root to:"home#index"
+  get "articles", to: "articles#index"
+  get "/articles/new", to: "articles#new", as: :new_articles
+  get "/articles/:id", to: "articles#show"
+  get "/articles/:id/edit", to: "articles#edit"
+
+  patch "/articles/:id", to: "articles#update", as: :article
+  delete "/articles/:id", to: "articles#delete"
+  post "articles", to:"articles#create"
+
 end
